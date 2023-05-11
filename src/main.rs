7491c420cf3c155db870dba5631374c62ce3565c
dayan <eam.dayan@gmail.com>
2023-05-11 11:34:06 +0700

    fix input prompt

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,33 @@
-use std::{io::stdin, process::exit};
+use std::{
+    io::{stdin, stdout, Write},
+    process::exit,
+};
+
+const EXIT_COMMAND: &str = ".exit";
+const EXIT_SUCCESS: i32 = 0;
 
 fn main() {
-    // let exit_command = String::from(".exit");
-    // let mut user_input;
+    let mut user_input;
 
-    // loop {
-    //     user_input = read_user_input();
+    loop {
+        user_input = read_user_input();
 
-    //     if user_input.trim() == exit_command {
-    //         exit(0)
-    //     } else {
-    //         println!("output {}", user_input);
-    //     }
-    // }
-
-    print_prompt();
-    // user_input = read_user_input();
-    let mut input_string = String::new();
-    stdin()
-        .read_line(&mut input_string)
-        .ok()
-        .expect("Failed to read line");
-
+        if user_input.trim() == EXIT_COMMAND {
+            exit(EXIT_SUCCESS)
+        } else {
+            print!("unrecognized command: {}", user_input);
+        }
+    }
 }
 
 fn print_prompt() {
     print!("db > ");
+    stdout().flush().unwrap(); // this needs to be flushed, otherwise it will only print after the input is complete
 }
 
 fn read_user_input() -> String {
     let mut input_string = String::new();
+    print_prompt();
     stdin()
         .read_line(&mut input_string)
         .ok()
